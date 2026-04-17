2b2e322ae392bfd32ec14b9062dfd26786b3ff58
Mlokeshnaidu <mandalalokesh1176@gmail.com>
2026-04-17 16:05:32 +0530

    first commit

---
+++ task1.js
@@ -0,0 +1,74 @@
+//variable update
+var a = 50;
+a=100;
+console.log(a);
+
+//let variable math
+let mark=80;
+result=mark+10;
+console.log(result);
+
+// const value
+const price = 500;
+let finalprice = price + 100;
+console.log(price);
+
+//printing statements
+console.log("Welcome Team");
+console.log(2026);
+console.log(true);
+
+//Datatype check
+console.log(typeof"JavaScript");
+console.log(typeof 250);
+console.log(typeof False);
+
+//Array Task
+let fruits = ["Banana","Mango","orange","apple","grapes"];
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits[fruits.length - 1]);
+
+//object Task
+let student = {
+    name:"naveen",
+    age:20,
+    course:"MERN"
+}
+console.log(student.name);
+console.log(student.age);
+
+//arithmetic operation Task
+console.log(20+10);
+console.log(50-25);
+console.log(5*5);
+console.log(100/4);
+console.log(20%3);
+
+// increment /decrement
+let x = 5;
+x++;
+console.log(x);
+
+let y = 10;
+y--;
+console.log(y);
+
+//comparison operators
+console.log(10>5);
+console.log(5<2);
+console.log(20=="20");
+console.log(15==="15");
+console.log(10!=8);
+
+
+//Logical OPerators
+console.log(5>2&&10>3);
+console.log(7<5||8>2);
+console.log(!(10>5));
+
+
+//Ternary Operator
+let age = 18;
+age>=18 ? console.log("Eligible") : console.log("0Not Eligible");
+
